2244c3ec2d8fedbeaee8d99a46af7336144fedd1
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:09:57 +0300

    Добавьте файлы проекта.

---
+++ zz8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        int testNumber = 17;
+        Console.WriteLine($"Число {testNumber} является простым: {IsPrime(testNumber)}");
+    }
+}
